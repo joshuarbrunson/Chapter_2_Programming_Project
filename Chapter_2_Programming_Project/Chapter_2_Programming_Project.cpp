e74f5fc4a27e99d933930fb212fd3c46ad8c9867
Joshua Brunson <joshuarbrunson@gmail.com>
2024-09-20 00:14:44 -0400

    Problem set 1 complete

--- Chapter_2_Programming_Project/Chapter_2_Programming_Project.cpp
+++ Chapter_2_Programming_Project/Chapter_2_Programming_Project.cpp
@@ -7,16 +7,18 @@ using namespace std;
 
 int main()
 {
-   
+	double growthRate = 1.5;
+	int initValue = 0;
+	cout << "Please provide the initial level of the ocean in milllimeters: ";
+	cin >> initValue;
+	cout << endl;
+	double fiveValue = (growthRate * 5) + initValue;
+	double sevenValue = (growthRate * 7) + initValue;
+	double tenValue = (growthRate * 10) + initValue;
+
+	cout << "The level of the ocean will be " << fiveValue << " millimeters after 5 years." << endl;
+	cout << "The level of the ocean will be " << sevenValue << " millimeters after 7 years." << endl;
+	cout << "The level of the ocean will be " << tenValue << " millimeters after 10 years." << endl;
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
